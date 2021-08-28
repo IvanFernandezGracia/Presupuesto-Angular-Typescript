4e549b18c5c2ebe0813f8ed1c6a93b242f199f21
Ivan Alejandro <ivan.fernandez.g@usach.cl>
2021-08-27 22:44:46 -0400

    files

---
+++ src/app/egreso/egreso.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
+
+@Component({
+  selector: 'app-egreso',
+  templateUrl: './egreso.component.html',
+  styleUrls: ['./egreso.component.css']
+})
+export class EgresoComponent implements OnInit {
+
+  egresos: Egreso[]=[];
+  @Input() ingresoTotal:number;
+
+  constructor(private egresoServicio: EgresoServicio) { }
+
+  ngOnInit(): void {
+    this.egresos = this.egresoServicio.egresos;
+  }
+
+  eliminarRegistro(egreso: Egreso){
+    this.egresoServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso: Egreso){
+    return egreso.valor/this.ingresoTotal;
+  }
+}
